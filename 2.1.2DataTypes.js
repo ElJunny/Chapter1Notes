0ecd411412ebc0603b4c28b3a349b6db8bc049a5
8nau <8nau@fresnoU.org>
2023-02-06 09:39:24 -0800

    initial.commit

---
+++ 2.1.2DataTypes.js
@@ -0,0 +1,47 @@
+console.log(26);
+//This is a comment
+
+//Numbers
+//Integers
+console.log(-12);
+console.log(0);
+console.log(12);
+//Floating Point Number
+console.log(2.63);
+console.log(100.045);
+
+//Strings
+//Characters, Letters, symbols, spaces
+console.log("hello world");
+console.log('HELLO WORLD');
+console.log(`hello WORLD`);
+console.log('16.0');
+
+//Arithmetic Operators (Math)
+console.log(14+2);
+console.log(14-2);
+console.log(31*4);  //shift-8 gives us *
+console.log(31/4);
+console.log(31%4);  //% is called MQD and means remainder after dividing
+
+//Concatenation: combine strings
+console.log("My name"+" is ugo")
+console.log(' My'+ '' + ' age ' + '' + ' 15 ' + '.')
+console.log("You're age \n 17");    // \n means new line
+
+//Template literal - plug in a calculated value
+//use backticks '' next to the 1
+console.log(`12 + 5 = ${12+5}`);
+console.log(`101 divided by 5 has the remainder of: ${101 % 5}`);
+console.log(`Your age is ${14+3}`);
+console.log(`200 divided by 8 equals ${200/8}`);
+console.log( 1 > 0 && 10 <= 100);
+console.log( 1 > 0 && 10 >= 100);
+console.log( 1 < 0 && 10 == 100);
+console.log( 1 > 0 || 10 <= 100);
+console.log( 1 > 0  || 10 >= 100);
+console.log( 1 < 0 || 10 == 100);
+console.log( 1 > 0 || 10 <= 100);
+console.log( 1 > 0  || 10 >= 100);
+console.log( 1 < 0 || 10 == 100);
+
---
+++ Operator.js
@@ -0,0 +1,7 @@
+console.log(typeof 15);
+console.log(typeof 'this');
+console.log(typeof "that" );
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof "TRUE");
+console.log( 1 > 0 && 10 <= 100);true
